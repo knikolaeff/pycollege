cf288dd679ea17ca75eb0ba4736521a92aea9b68
knikolaeff <kir1llnikolaeff@yandex.by>
2021-10-01 00:02:55 +0100

    Revert "Formatting"
    This reverts commit 309dfff84aa6f3bcb397e223c67e6bfac5af5a6a.
    
    tried reverting :)

--- location.py
+++ location.py
@@ -1,8 +1,10 @@
 import requests
 
+
 def get_location():
     ip = requests.get("http://jsonip.com").json()["ip"]
     jsondata = requests.get("http://ip-api.com/json/" + ip).json()
     return jsondata
+
 
 print(get_location())
