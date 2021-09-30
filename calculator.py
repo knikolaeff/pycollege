8a1a9212e4b482229fc9fef44373c12397d9740a
knikolaeff <kir1llnikolaeff@yandex.by>
2021-09-30 23:44:33 +0100

    Translated to English and added error handling

--- calculator.py
+++ calculator.py
@@ -1,6 +1,20 @@
 import argparse
+
+parser = argparse.ArgumentParser(description = "Available operations are: +, -, /, *, mod, pow, div")
+args = parser.parse_args() 
+
+while True:
+    try:
+        n1 = float(input('Enter your first value: '))
+        operation = input('Enter operation: ')
+        n2 = float(input('Enter your second value: '))
+        break
+    except ValueError:
+        print("Only digits allowed as a value!")
+        continue
+
 if (operation == '/' or operation == 'mod' or operation == 'div') and n2 == 0:
-    print('Деление на 0!')
+    print('Division by zero!')
 elif operation == '+':  print(n1 + n2) 
 elif operation == '-':  print(n1 - n2)
 elif operation == '/':  print(n1 / n2)
@@ -8,3 +22,5 @@ elif operation == '*':  print(n1 * n2)
 elif operation == 'mod':    print(n1 % n2)
 elif operation == 'pow':    print(n1 ** n2)
 elif operation == 'div':    print(n1 // n2)
+
+   
