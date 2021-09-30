309dfff84aa6f3bcb397e223c67e6bfac5af5a6a
knikolaeff <kir1llnikolaeff@yandex.by>
2021-09-30 23:57:31 +0100

    Formatting

--- location.py
+++ location.py
@@ -1,10 +1,8 @@
 import requests
 
-
 def get_location():
     ip = requests.get("http://jsonip.com").json()["ip"]
     jsondata = requests.get("http://ip-api.com/json/" + ip).json()
     return jsondata
-
 
 print(get_location())
