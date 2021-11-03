618ec1c6075f0975cab1ba3a697035254922ac47
NIK21004887 - Kiryl Nikalayeu <NIK21004887@wakecoll.net>
2021-11-03 11:57:08 +0000

    Comments and final formatting before submitting

--- GUICalculator2.0.py
+++ GUICalculator2.0.py
@@ -1,38 +1,38 @@
 import tkinter as tk
 import math
 import sys
-
+# Creating a window
 window = tk.Tk()
 window.title("Calculator")
 window.configure(bg="#72a3ea")
+# The window looks differently on different OS so I specify sizes here
 if sys.platform.startswith('win'):
     window.geometry("380x395")
 else:
     window.geometry('500x460')
-
+# This entry will show all the user input
 entry = tk.Entry(window, width=50,  borderwidth=4, bg="#d8fcff", fg="black")
 entry.grid(row=0, column=0, columnspan=4, ipady=5, pady=5)
 
 is_extra_clicked = False
-action = None
-first_number = None
 
-
+# This function will clean the entry 
 def clean_input():
     entry.delete(0, "end")
 
-
+# "Click" function registers a button user clicks and shows it's value. 
 def click(number):
 
     curValue = entry.get()
     clean_input()
     entry.insert(0, curValue + str(number))
 
+# Using the sqrt method from math package, this function calculates square root of an entered value
 def root(number):
     clean_input()
     entry.insert(0, math.sqrt(float(number)))
 
-
+# This function toggles "extra" tab by showing and hiding the buttons on the right
 def extra():
 
     global is_extra_clicked
@@ -57,7 +57,9 @@ def extra():
         button_opBracket.grid_forget()
         button_clBracket.grid_forget()
 
-
+# This function calculates input in the entry and prints the result. Also, it handles errors
+# Eval is a dangerous method, because it executes everything inside the entry without filtering it,
+# but this program will work only on user's machine so using is justified 
 def equal():
 
     try:
@@ -74,19 +76,18 @@ def equal():
         clean_input()
         entry.insert(0, "The number is too big!")
 
-
-
+# This function declares buttons from 1 to 9. Every attribute of these buttons is the same except command and text 
 def make_cell_buttons():
-    arr = []
+    lst = []
     for t in range(1, 10):
         new_btn = tk.Button(window, text=str(t),
                             width=12, height=4,
                             bg="#72a3ea", fg="#000000",
                             command=lambda x=t: click(x))
-        arr.append(new_btn)
-    return arr
+        lst.append(new_btn)
+    return lst
 
-
+# Declaring all the extra buttons manually because sizes are "hit or miss" in tkinter 
 buttons1_9 = make_cell_buttons()
 
 button0 = tk.Button(window, text="0", width=26, height=4,
@@ -103,25 +104,32 @@ button_clear = tk.Button(window, text="Clear", width=26, height=4,
 
 button_divide = tk.Button(window, text="/", width=12, height=4,
                           bg="#7285ea", fg="black", command=lambda: click("/"))
+
 button_multiply = tk.Button(window, text="*", width=12, height=4,
                             bg="#7285ea", fg="black", command=lambda: click("*"))
+
 button_subtract = tk.Button(window, text="-", width=12, height=4,
                             bg="#7285ea", fg="black", command=lambda: click("-"))
+
 button_dot = tk.Button(window, text=".", width=12, height=4,
                        bg="#72a3ea", fg="black", command=lambda: click("."))
+
 button_extra = tk.Button(window, text="Extra", width=12, height=4,
                          bg="#7285ea", fg="black", command=lambda: extra())
 
 button_power = tk.Button(window, text="bⁿ", width=12, height=4,
                          bg="#7285ea", fg="black", command=lambda: click(" ** "))
+
 button_root = tk.Button(window, text="√", width=12, height=4,
                         bg="#7285ea", fg="black", command=lambda: root(entry.get()))
+
 button_clBracket = tk.Button(window, text=")", width=12, height=4,
                          bg="#7285ea", fg="black", command=lambda: click(")"))
+
 button_opBracket = tk.Button(window, text="(", width=12, height=4,
                          bg="#7285ea", fg="black", command=lambda: click("("))
 
-
+# Using dictionary, filling buttons from 1 to 9 into grid
 def fill_positions(buttons):
 
     buttonRowCol = {
@@ -142,6 +150,7 @@ def fill_positions(buttons):
 
 fill_positions(buttons1_9)
 
+# Filling everything that left
 button_divide.grid(row=1, column=3)
 button_multiply.grid(row=2, column=3)
 button_subtract.grid(row=3, column=3)
@@ -155,5 +164,6 @@ button_clear.grid(row=5, column=0, columnspan=2)
 button_extra.grid(row=5, column=2)
 button_equal.grid(row=5, column=3)
 
+# This will not break the window by accidentaly resizing it
 window.resizable(False, False)
 window.mainloop()
