132b67104d8f5d3786669c8330e348b9bc250d56
bipolarus <kir1llnikolaeff@yandex.by>
2021-11-04 16:44:18 +0000

    Updates

--- GUICalculator2.0.py
+++ GUICalculator2.0.py
@@ -136,15 +136,15 @@ button_opBracket = tk.Button(window, text="(", width=12, height=4,
 def fill_positions(buttons):
 
     buttonRowCol = {
-        0: [1, 0],
-        1: [1, 1],
-        2: [1, 2],
+        0: [3, 0],
+        1: [3, 1],
+        2: [3, 2],
         3: [2, 0],
         4: [2, 1],
         5: [2, 2],
-        6: [3, 0],
-        7: [3, 1],
-        8: [3, 2],
+        6: [1, 0],
+        7: [1, 1],
+        8: [1, 2],
     }
 
     for i, val in enumerate(buttons):
