407fcc6ec263b664de6b96dfdcae7c24985f17c4
bipolarus <kir1llnikolaeff@yandex.by>
2021-12-03 21:30:45 +0000

    Made a prototype

--- parking.py
+++ parking.py
@@ -1,40 +1,75 @@
 import tkinter as tk
+from tkinter.constants import END
 
-regplates = []
+
 window = tk.Tk()
+window.title("Parking App")
+
 interfaceFrame = tk.Frame(window)
 interfaceFrame.pack()
+
 displayFrame = tk.Frame(window)
 displayFrame.pack()
 
+canvas = tk.Canvas(master=displayFrame, width=600, height=230)
+canvas.pack()
+
+
+class ParkingLot:
+    def __init__(self, x1, y1, x2, y2, number):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.number = number
+        canvas.create_rectangle(x1, y1, x2, y2)
+        canvas.create_text(x1 + 55, y2 + 10, text="Lot %s" % (number))
+
+    def TakeLot(self):
+        self.regplate = canvas.create_text(self.x1 + 55, self.y2 +
+                                           40, text=regplateEnt.get())
+        regplateEnt.delete(0, END)
+        lotEnt.delete(0, END)
+
+    def EmptyLot(self):
+        canvas.delete(self.regplate)
+        regplateEnt.delete(0, END)
+        lotEnt.delete(0, END)
+
+
+parkingLots = {
+    1: ParkingLot(5, 135, 115, 65, 1),
+    2: ParkingLot(125, 135, 235, 65, 2),
+    3: ParkingLot(245, 135, 355, 65, 3),
+    4: ParkingLot(365, 135, 475, 65, 4),
+    5: ParkingLot(485, 135, 595, 65, 5),
+    6: ParkingLot(5, 215, 115, 145, 6),
+    7: ParkingLot(125, 215, 235, 145, 7),
+    8: ParkingLot(245, 215, 355, 145, 8),
+    9: ParkingLot(365, 215, 475, 145, 9),
+    10: ParkingLot(485, 215, 595, 145, 10),
+}
+
 regplateLbl = tk.Label(master=interfaceFrame, text="Registration number")
-regplateLbl.grid(row=0, column=2)
+regplateLbl.grid(row=0, column=1)
+
+lotLbl = tk.Label(master=interfaceFrame, text="Lot")
+lotLbl.grid(row=0, column=4)
 
 warningLbl = tk.Label(master=interfaceFrame, text="Full!", fg="red")
 
-regplateEnt = tk.Entry(master=interfaceFrame, width=50)
-regplateEnt.grid(row=1, column=0, columnspan=4, padx=5, pady=5)
+regplateEnt = tk.Entry(master=interfaceFrame, width=30)
+regplateEnt.grid(row=1, column=0, columnspan=3, padx=5, pady=5)
 
-regplateSave = tk.Button(master=interfaceFrame, text="Save", padx=5, command=lambda: saveplate(regplateEnt.get())) 
-regplateSave.grid(row=1, column=5, padx=5)
+lotEnt = tk.Entry(master=interfaceFrame, width=5)
+lotEnt.grid(row=1, column=4)
 
-regplateInfo = tk.Button(master=interfaceFrame, text="Display all the cars", command=lambda: displayplates())
-regplateInfo.grid(row=1, column=6, padx=5)
+saveBtn = tk.Button(master=interfaceFrame, text="Save",
+                    padx=5, command=lambda: parkingLots[int(lotEnt.get())].TakeLot())
+saveBtn.grid(row=1, column=5, padx=5)
 
-def displayplates():
-    for widget in displayFrame.winfo_children():
-        widget.destroy()
-
-    for i in regplates:
-        plate = tk.Label(master=displayFrame, text=i)
-        plate.pack()
-
-
-def saveplate(plate):
-    regplates.append(plate)
-    regplateEnt.delete(0, tk.END)
-  
-    if len(regplates) == 10:
-        warningLbl.grid(row=0, column=5)
+removeBtn = tk.Button(master=interfaceFrame, text="Remove", padx=5,
+                      command=lambda: parkingLots[int(lotEnt.get())].EmptyLot())
+removeBtn.grid(row=1, column=6, padx=5)
 
 tk.mainloop()
