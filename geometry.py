5685d0855c6e9cff6ac2eba8f878af2c5cd26af2
Kirill Nikolaev <80189148+knikolaeff@users.noreply.github.com>
2022-02-07 13:46:50 +0000

    Localisation

--- geometry.py
+++ geometry.py
@@ -1,20 +1,20 @@
-print('Введите фигуру: треугольник, прямоугольник или круг')
-shape = input('Фигура: \n')
+print('Enter your shape: a triangle, a rectangle or a circle')
+shape = input('Shape: \n')
 shape = shape.lower()
-if shape == 'треугольник':
-    print('Введите стороны треугольника')
+if shape == 'triangle':
+    print('Enter sides of the triangle')
     a = int(input())
     b = int(input())
     c = int(input())
     p = (a + b + c) / 2
     S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
-elif shape == 'прямоугольник':
-    print('Введите стороны прямоугольника')
+elif shape == 'rectangle':
+    print('Enter sides of the rectangle')
     a = int(input())
     b = int(input())
     S = a * b 
-elif shape == 'круг':
-    print('Введите радиус круга')
+elif shape == 'circle':
+    print('Enter radius of the circle')
     r = int(input())
     S = 3.14 * r ** 2 
 print(S)
